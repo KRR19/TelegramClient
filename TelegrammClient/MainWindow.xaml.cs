beb45c8f1df98f5483c29467aaf4e02d183de5d7
Roman Kazarov <roman.kazarov@infopulse.com>
2020-07-21 09:30:21 +0300

    Add project files.

---
+++ TelegrammClient/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegrammClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<TelegramUser> Users;
+        TelegramBotClient bot;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Users = new ObservableCollection<TelegramUser>();
+            userList.ItemsSource = Users;
+
+            string token = System.IO.File.ReadAllText("token");
+            bot = new TelegramBotClient(token);
+
+            bot.OnMessage += delegate (object sender, Telegram.Bot.Args.MessageEventArgs e)
+            {
+                string msg = $"{DateTime.Now}: {e.Message.Chat.FirstName}, {e.Message.Chat.Id}, {e.Message.Text}";
+                System.IO.File.AppendAllText("data.log", $"{msg}\n");
+                Debug.WriteLine(msg);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    var persone = new TelegramUser(e.Message.Chat.FirstName, e.Message.Chat.Id);
+                    if (!Users.Contains(persone)) Users.Add(persone);
+                    Users[Users.IndexOf(persone)].AddMessage($"{persone.Nick}: {e.Message.Text}");
+                });
+            };
+                bot.StartReceiving();
+
+                btnSendMsg.Click += delegate { SendMsg(); };
+                txtBxSendMsg.KeyDown += (s, e) => { if (e.Key == Key.Return) { SendMsg(); } };
+            
+        }
+
+        public void SendMsg()
+        {
+            var currentUser = Users[Users.IndexOf(userList.SelectedItem as TelegramUser)];
+            string responseMsg = $"Support: {txtBxSendMsg.Text}";
+            currentUser.Message.Add(responseMsg);
+
+            bot.SendTextMessageAsync(currentUser.Id, txtBxSendMsg.Text);
+            string logText = $"{ DateTime.Now}: >> {currentUser.Id} {currentUser.Nick} {responseMsg}\n ";
+            System.IO.File.AppendAllText("data.log", logText);
+
+            txtBxSendMsg.Text = string.Empty;
+        }
+
+    }
+}
---
+++ TelegrammClient/TelegramUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace TelegrammClient
+{
+    public class TelegramUser : INotifyPropertyChanged, IEquatable<TelegramUser>
+    {
+        private string nick;
+        private long id;
+        public string Nick { 
+            get
+            {
+                return this.nick;
+            }
+            set 
+            {
+                this.nick = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.nick)));
+            }
+        }
+        public long Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.id)));
+            }
+        }
+        public ObservableCollection<string> Message { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TelegramUser(string nick, long id)
+        {
+            this.Id = id;
+            this.Nick = nick;
+            this.Message = new ObservableCollection<string>();
+        }
+
+        public bool Equals(TelegramUser other) => other.Id == this.Id;
+        public void AddMessage(string Text) => Message.Add(Text);
+    }
+}
